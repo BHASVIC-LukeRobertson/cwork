cd2191c1412838142a2f3609545c03ffa3ccb5ea
BHASVIC-LukeRobertson <145350474+BHASVIC-LukeRobertson@users.noreply.github.com>
2023-09-18 13:40:07 +0100

    ?

---
+++ chapter 2/exercise 1.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+int main()
+
+{
+    srand(time(NULL)); // initialise random num generator
+
+
+    int num1 = rand() % 10+1; // randomise NUM1
+
+    int ges = 0; // declare guess variable
+
+    printf("\n Guess a number between 1 and 10: \n");
+
+    scanf("%i",ges); // get guess input
+
+    if (ges == num1){
+        printf("Correct");
+    }
+
+    else{
+        printf("Wrong");
+    }
+
+    return 0;
+}
